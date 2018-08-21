c52aa9a8e13482a3d1115fece9ab66e21b1a56c1
Ai Nguyen <ainguyenkaka@gmail.com>
2018-08-21 13:10:16 +0800

    update color property with hex string

--- core/property/color.go
+++ core/property/color.go
@@ -1,8 +1,20 @@
 package property
 
+import "github.com/lucasb-eyer/go-colorful"
+
 // Color is the property of component.
 type Color struct {
 	R uint8
 	G uint8
 	B uint8
+}
+
+// Hex returns hexdecimal value.
+func (color *Color) Hex() string {
+	colorful := colorful.Color{
+		R: float64(color.R / 255),
+		G: float64(color.G / 255),
+		B: float64(color.B / 255),
+	}
+	return colorful.Hex()
 }
--- core/property/color_test.go
+++ core/property/color_test.go
@@ -1,7 +1,10 @@
 package property_test
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 
 	"github.com/stretchr/testify/assert"
 
@@ -17,4 +20,24 @@ func TestNewColor(t *testing.T) {
 	assert.Equal(t, uint8(0), color.R)
 	assert.Equal(t, uint8(0), color.G)
 	assert.Equal(t, uint8(0), color.B)
+}
+
+type ColorTestSuite struct {
+	suite.Suite
+	color *property.Color
+}
+
+func (suite *ColorTestSuite) SetupTest() {
+	suite.color = new(property.Color)
+}
+
+func (suite *ColorTestSuite) TestHex() {
+	hex := suite.color.Hex()
+	suite.NotNil(hex)
+	suite.True(len(hex) > 0)
+	suite.True(strings.Contains(hex, "#"))
+}
+
+func TestColorTestSuite(t *testing.T) {
+	suite.Run(t, new(ColorTestSuite))
 }
